98a31a6e44aff1f94b77a225eae17a9bcc2bcdd8
09.02.07 <il.ruzanovil@yandex.ru>
2023-12-15 11:01:21 +0300

    Comment

--- app/src/main/java/com/example/killmonstr/MainActivity.java
+++ app/src/main/java/com/example/killmonstr/MainActivity.java
@@ -81,15 +81,12 @@ public class MainActivity extends AppCompatActivity {
                 counterHp.setText(counterHP.toString());
 
                 if (counterHP <=0) {
-                    Toast.makeText(MainActivity.this, "Вы выиграли!", Toast.LENGTH_SHORT).show();
-                    button.setEnabled(false);
+                    Toast.makeText(MainActivity.this, "Вы выиграли!", Toast.LENGTH_SHORT).show();//вывод сообщения о победе
+                    button.setEnabled(false); //блокировка кнопки после победы
                 }
 
             }
         });
-
-
-
         mTimerText = findViewById(R.id.textView4);
 
 
@@ -124,10 +121,12 @@ public class MainActivity extends AppCompatActivity {
             public void onFinish() {
                 mTimerText.setText("End");
                 findViewById(R.id.button_s).setEnabled(false);
-                if (counterHP <=0) {
-
-                }
-
+                findViewById(R.id.button5).setEnabled(false);
+                findViewById(R.id.button4).setEnabled(false);
+                findViewById(R.id.button7).setEnabled(false);
+                findViewById(R.id.button3).setEnabled(false);
+                findViewById(R.id.button6).setEnabled(false);
+                findViewById(R.id.button2).setEnabled(false);
             }
 
 
@@ -141,64 +140,64 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void onClickBtn2AddDex(View view) {
-        counter = counter - 100; // Уменьшение значения счетчика на 10
+        counter = counter - 100; // Уменьшение значения счетчика на 100
         counterbutton =+5 + counterbutton; // Увеличение значения кнопки на 5
         TextView counterView = findViewById(R.id.scoreView);
         counterView.setText(counter.toString());
         counterHPZoom =- counterbutton ;
         if (counter < 100) {
-            findViewById(R.id.button5).setEnabled(false); // Выключение кнопки, если значение счетчика меньше 10
+            findViewById(R.id.button5).setEnabled(false); // Выключение кнопки, если значение счетчика меньше 100
         }
 
     }
 
     public void onClickBtn4AddDex(View view) {
-        counter = counter - 250; // Уменьшение значения счетчика на 10
-        counterbutton =+15 + counterbutton; // Увеличение значения кнопки на 10
+        counter = counter - 250; // Уменьшение значения счетчика на 250
+        counterbutton =+15 + counterbutton; // Увеличение значения кнопки на 15
         TextView counterView = findViewById(R.id.scoreView);
         counterView.setText(counter.toString());
 
         if (counter < 250) {
-            findViewById(R.id.button6).setEnabled(false); // Выключение кнопки, если значение счетчика меньше 10
+            findViewById(R.id.button6).setEnabled(false); // Выключение кнопки, если значение счетчика меньше 250
         }
     }
     public void onClickBtn5AddDex(View view) {
-        counter = counter - 350; // Уменьшение значения счетчика на 10
-        counterbutton =+20 + counterbutton; // Увеличение значения кнопки на 5
+        counter = counter - 350; // Уменьшение значения счетчика на 350
+        counterbutton =+20 + counterbutton; // Увеличение значения кнопки на 20
         TextView counterView = findViewById(R.id.scoreView);
         counterView.setText(counter.toString());
 
         if (counter < 350) {
-            findViewById(R.id.button2).setEnabled(false); // Выключение кнопки, если значение счетчика меньше 10
+            findViewById(R.id.button2).setEnabled(false); // Выключение кнопки, если значение счетчика меньше 350
         }
     }
     public void onClickBtn6AddDex(View view) {
-        counter = counter - 440; // Уменьшение значения счетчика на 10
-        counterbutton =+25 + counterbutton; // Увеличение значения кнопки на 5
+        counter = counter - 440; // Уменьшение значения счетчика на 440
+        counterbutton =+25 + counterbutton; // Увеличение значения кнопки на 25
         TextView counterView = findViewById(R.id.scoreView);
         counterView.setText(counter.toString());
         if (counter < 440) {
-            findViewById(R.id.button3).setEnabled(false); // Выключение кнопки, если значение счетчика меньше 10
+            findViewById(R.id.button3).setEnabled(false); // Выключение кнопки, если значение счетчика меньше 440
         }
     }
     public void onClickBtn7AddDex(View view) {
-        counter = counter - 500; // Уменьшение значения счетчика на 10
-        counterbutton =+30 + counterbutton; // Увеличение значения кнопки на 5
+        counter = counter - 500; // Уменьшение значения счетчика на 500
+        counterbutton =+30 + counterbutton; // Увеличение значения кнопки на 30
         TextView counterView = findViewById(R.id.scoreView);
         counterView.setText(counter.toString());
         if (counter < 500) {
-            findViewById(R.id.button7).setEnabled(false); // Выключение кнопки, если значение счетчика меньше 10
+            findViewById(R.id.button7).setEnabled(false); // Выключение кнопки, если значение счетчика меньше 500
         }
     }
 
 
     public void nClickBtn3AddDex(View view) {
-        counter = counter - 180; // Уменьшение значения счетчика на 10
-        counterbutton =+10 + counterbutton; // Увеличение значения кнопки на 5
+        counter = counter - 180; // Уменьшение значения счетчика на 180
+        counterbutton =+10 + counterbutton; // Увеличение значения кнопки на 10
         TextView counterView = findViewById(R.id.scoreView);
         counterView.setText(counter.toString());
         if (counter < 180) {
-            findViewById(R.id.button4).setEnabled(false); // Выключение кнопки, если значение счетчика меньше 10
+            findViewById(R.id.button4).setEnabled(false); // Выключение кнопки, если значение счетчика меньше 180
         }
 
     }
